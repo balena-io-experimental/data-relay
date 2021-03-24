160565e42884f90b3e6917febac8be464005ae3f
Phil Wilson <39430324+phil-d-wilson@users.noreply.github.com>
2021-03-24 12:38:52 +0000

    MQTT input WIP

--- Azure/sensor/run.sh
+++ Azure/sensor/run.sh
@@ -2,6 +2,6 @@
 
 python3 ./src/autoconfigure.py
 sleep 3
-daprd --components-path /app/components --app-id $1 &
+daprd --components-path /app/components --app-protocol grpc --dapr-grpc-port=3500 --app-id $1 &
 sleep 3
 python3 ./src/main.py 
