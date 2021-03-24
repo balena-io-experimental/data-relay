bd1968036dac5484422cb808ab575c121b28dd63
Phil Wilson <39430324+phil-d-wilson@users.noreply.github.com>
2021-03-24 13:48:06 +0000

    MQTT input working

--- sensor/src/main.py
+++ sensor/src/main.py
@@ -1,9 +1,10 @@
 import time
 import requests
 import os
+import json
 
 from cloudevents.sdk.event import v1
-from dapr.ext.grpc import App
+from dapr.ext.grpc import App, BindingRequest
 
 import json
 
@@ -13,20 +14,20 @@ dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 blob_url = "http://localhost:{}/v1.0/bindings/blob".format(dapr_port)
 bindings = [blob_url]
 
-@app.subscribe(pubsub_name='mqtt-input', topic='cloud-input')
-def mytopic(event: v1.Event) -> None:
-    data = json.loads(event.Data())
-    print(f'Subscriber received: id={data["id"]}, message="{data["message"]}", content_type="{event.content_type}"',flush=True)
-
+@app.binding('mqtt-input')
+def binding(request: BindingRequest):
+    print(request.text(), flush=True)
     try:
         for url in bindings:
-            response = requests.post(url, json=data)
+            response = requests.post(url, json=json.dumps(request.text()))
             print(response, flush=True)
 
     except Exception as e:
         print(e, flush=True)
 
 app.run(50051)
+
+
 
 
 
