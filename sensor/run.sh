b35b19a330f3e18075f758e219c626212667598b
Michal Toman <michalt@balena.io>
2021-03-25 11:52:59 +0100

    Kill dapr after getSecrets
    Signed-off-by: Michal Toman <michalt@balena.io>

--- sensor/run.sh
+++ sensor/run.sh
@@ -4,8 +4,11 @@ if [ "$SECRETS" = "KEYVAULT" ];then
     python3 ./src/autoconfigureSecrets.py 
     sleep 3
     daprd --components-path /app/components --app-id $1 &
+    DAPR_PID=$!
     sleep 3
     python3 ./src/getSecrets.py #generates environ variables with the secrets
+    kill -SIGTERM "$DAPR_PID"
+    sleep 3
 fi
 
 python3 ./src/autoconfigure.py
