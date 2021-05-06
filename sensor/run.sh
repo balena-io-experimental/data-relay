243621039d4a05f61e32cba5d76e16cac8d72145
Ken Bannister <kb2ma@runbox.com>
2021-05-06 09:07:14 -0400

    Disable reading secrets plugins for now
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- sensor/run.sh
+++ sensor/run.sh
@@ -5,19 +5,7 @@ then
     DAPR_LOGLEVEL="--log-level debug"
 fi
 
-# Load information from secrets plugins
-if python3 ./src/autoconfigure.py secrets
-then
-    sleep 3
-    daprd $DAPR_LOGLEVEL --components-path /app/components/secrets --app-id $1 &
-    DAPR_PID=$!
-    sleep 3
-    `python3 ./src/getSecrets.py` # environment variables are applied from stdout
-    kill -SIGTERM "$DAPR_PID"
-    sleep 5
-fi
-
-# Initialize input and out plugins
+# Initialize dapr services from plugins
 python3 ./src/autoconfigure.py
 sleep 3
 daprd $DAPR_LOGLEVEL --components-path /app/components --app-protocol grpc --app-port 50051 --app-id $1 &
