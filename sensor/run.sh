54b73ff0179de56f11e1bf15a4d517324cd5e341
Ken Bannister <kb2ma@runbox.com>
2021-05-18 09:19:05 -0400

    Use tmpfs for /app/components dapr component config files
    More secure than writing to disk/flash.
    
    Change-type: patch
    Signed-off-by: Ken Bannister <kb2ma@runbox.com>

--- sensor/run.sh
+++ sensor/run.sh
@@ -5,6 +5,18 @@ then
     DAPR_LOGLEVEL="--log-level debug"
 fi
 
+# Ensure directory set up for component configuration files. By default use
+# tmpfs to avoid writing secrets to disk/flash. However, allow containing
+# Dockerfile/docker-compose to override setup here, for example for development.
+component_dir=/app/components
+if [ -e  "$component_dir" ]
+then
+    echo "$component_dir directory already exists"
+else
+    mkdir "$component_dir"
+    mount -t tmpfs -o mode=711 tmpfs "$component_dir"
+fi
+
 # Initialize dapr services from plugins
 python3 ./src/autoconfigure.py
 sleep 3
